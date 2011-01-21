a07a427000c0765731132805c691ef5902863515
Sean Kelly <github@katron.org>
2011-01-20 21:53:54 -0600

    Oops, extra ]

--- trades.js
+++ trades.js
@@ -115,7 +115,7 @@ Transaction.prototype.get_transaction_types = function(playerid) {
 
     var types = []
     for (var i = 0; i < player.length; i++) {
-        types.push(player[i].type]);
+        types.push(player[i].type);
     }
 
     return types;
