8b8b34ba26f382014a0f9d4b2c06ef205457b23a
Sean Kelly <github@katron.org>
2011-05-15 21:17:40 -0500

    Also load the BDB Master.txt file

--- parse_trans.pl
+++ parse_trans.pl
@@ -3,42 +3,56 @@ use strict;
 use warnings;
 use 5.010;
 
+use JSON::Any;
 use Text::CSV;
 
+die "Usage: $0 tran.txt Master.txt\n" unless @ARGV == 2;
+
 my $csv = Text::CSV->new;
-my $csvout = Text::CSV->new;
-$csvout->eol("\n");
-my ($file);
+my $json = JSON::Any->new;
+my %players;
 
-for $file (@ARGV) {
-    open my $fh, '<', $file or die "Could not open $file: $!";
-    
-    my @rows;
-    while (my $row = $csv->getline($fh)) {
-        my @r = @{ $row };
-        my ($y, $m, $d);
-        if (length($r[0]) > 0) {
-            ($y, $m, $d) = $r[0] =~ /^(\d{4})(\d{2})(\d{2})/;
-            $d = "01" if $d eq "00";
-            $m = "01" if $m eq "00";
-            $r[0] = "$y-$m-$d";
-        }
-        if (length($r[3]) > 0) {
-            ($y, $m, $d) = $r[3] =~ /^(\d{4})(\d{2})(\d{2})/;
-            $d = "01" if $d eq "00";
-            $m = "01" if $m eq "00";
-            $r[3] = "$y-$m-$d";
-        }
+open my $retro_transactions, '<', $ARGV[0] or die "Could not open $ARGV[0]: $!";
+open my $bdb_master, '<', $ARGV[1] or die "Could not open $ARGV[1]: $!";
 
-        $r[2] = "true" if $r[2] eq "@";
-        $r[4] = "true" if $r[4] eq "@";
+my @r;
+my $row;
+# Load BDB players first. What I want is the bbref ID, the player's
+# first name, last name, and full name.
+$row = $csv->getline($bdb_master);
+# Only do the column check once.
+die "BDB Master file not detected.\n" unless @{ $row } == 33;
+do {
+    @r = @{ $row };
+} while ($row = $csv->getline($bdb_master));
 
-        # Rearrange columns some and insert a new one for bbrefid.
-        @r = ($r[5], @r[0..4,6], "", @r[7..$#r]);
-
-        push @rows, \@r;
+# Now load the transactions list from Retrosheet.
+$row = $csv->getline($retro_transactions);
+die "Retrosheet transactions fine not detected.\n" unless @{ $row } == 16;
+do {
+    @r = @{ $row };
+    my ($y, $m, $d);
+    if (length($r[0]) > 0) {
+        ($y, $m, $d) = $r[0] =~ /^(\d{4})(\d{2})(\d{2})/;
+        $d = "01" if $d eq "00";
+        $m = "01" if $m eq "00";
+        $r[0] = "$y-$m-$d";
     }
-    close $fh;
+    if (length($r[3]) > 0) {
+        ($y, $m, $d) = $r[3] =~ /^(\d{4})(\d{2})(\d{2})/;
+        $d = "01" if $d eq "00";
+        $m = "01" if $m eq "00";
+        $r[3] = "$y-$m-$d";
+    }
 
-    $csvout->print(*STDOUT, $_) for @rows;
-}
+    $r[2] = "true" if $r[2] eq "@";
+    $r[4] = "true" if $r[4] eq "@";
+
+    # Rearrange columns some and insert a new one for bbrefid.
+    @r = ($r[5], @r[0..4,6], "", @r[7..$#r]);
+
+#    push @rows, \@r;
+} while ($row = $csv->getline($retro_transactions));
+
+close $retro_transactions;
+close $bdb_master;
